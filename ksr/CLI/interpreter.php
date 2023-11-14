b5b502865df8b0dee731e2ac3994683aae4e0d70
Cosmo <58365407+l0th3r@users.noreply.github.com>
2023-11-14 14:09:41 +0100

    3 reorganise interpreter to be external of project (#4)
    * moved interpreter outside of src
    
    * removed repl command
    
    * fix namespace for interpreter

---
+++ ksr/CLI/interpreter.php
@@ -0,0 +1,102 @@
+<?php
+namespace Ksr\CLI;
+
+use Error;
+use Exception;
+use Symfony\Component\Console\Application;
+
+class Interpreter extends Application
+{
+    protected string $filePath = "";
+    protected string $commandsNamespace = "";
+
+    public function __construct(string $appName, string $commandFilePath)
+    {
+        parent::__construct();
+
+        $this->setName($appName);
+        $this->filePath = $commandFilePath;
+
+        try
+        {
+            $this->importCommands();
+        }
+        catch (Exception $ex)
+        {
+            echo $this->getName() . " cli error: " . $ex->getMessage() . "\n";
+            return;
+        }
+
+        try
+        {
+            $this->run();
+        }
+        catch (Exception $ex)
+        {
+            echo $this->getName() . " error: " . $ex->getMessage() . "\n";
+            return;
+        }
+    }
+
+    protected function importCommands()
+    {
+        if(is_dir($this->filePath) || !is_file($this->filePath))
+        {
+            throw new Exception("Command file path is not properly set.");
+        }
+
+        $lineIdx = 0;
+        foreach($this->getFileLines($this->filePath) as $line)
+        {
+            $lContent = preg_replace("/\r|\n/", "", $line);
+            
+            if($lineIdx == 0)
+            {
+                $this->commandsNamespace = $lContent . "\\";
+            }
+            else
+            {
+                $commandClass = $this->commandsNamespace . ucfirst($lContent);
+
+                try
+                {
+                    $this->add(new $commandClass());
+                }
+                catch(Error $err)
+                {
+                    throw new Exception("Could not find command class '" . $commandClass . "' from file " . $this->filePath);
+                }
+            }
+
+            $lineIdx++;
+        }
+    }
+
+    protected function assignCommand(string $commandClass)
+    {
+        $this->add(new $commandClass());
+    }
+
+    protected function getFileLines($path)
+    {
+        $file = fopen($path, "r");
+        
+        if($file == false)
+        {
+            fclose($file);
+            throw new Exception("Could not open command file at " . $path);
+        }
+
+        $lines = [];
+
+        while (($line = fgets($file)) !== false) {
+            array_push($lines, $line);
+        }
+
+        fclose($file);
+
+        return $lines;
+    }
+}
+
+?>
