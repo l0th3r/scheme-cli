1a37ed0096a6d6313afa8bf921a6a27c8d445f69
Cosmo <58365407+l0th3r@users.noreply.github.com>
2023-11-23 20:07:03 +0100

    22 handle division by 0 (#23)
    * fix division by 0
    
    * review command description

--- src/Command/read.php
+++ src/Command/read.php
@@ -13,7 +13,7 @@ class Read extends Command
     protected function configure()
     {
         $this->setName("read");
-        $this->setDescription("Read scheme language declaration and return result");
+        $this->setDescription("Read scheme language declaration. Will print errors and results");
         $this->setHelp("Use double quotes to write a space-separated declaration.\nExemple: \"(+ 5 10) (* 5 2)\" as declaration will output:\n15\n10");
 
         $this->addArgument("declaration", InputArgument::REQUIRED, 'Scheme declaration to execute');
